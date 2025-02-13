7abc407744de8b44c726b13d067fdcb701681e2f
Droingo <cmbmx57@gmail.com>
2025-02-14 09:05:26 +1000

    Initial commit

---
+++ src/main/java/net/droingo/droingomaze/DroingoMazeClient.java
@@ -0,0 +1,10 @@
+package net.droingo.droingomaze;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DroingoMazeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
